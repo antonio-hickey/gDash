0622778a308231cf7959b9b063069afafbefa2ff
Antonio Hickey <antoniohickey99@gmail.com>
2021-04-13 21:13:17 -0400

    Automated Data Collection

--- data/covid/covid_miner.py
+++ data/covid/covid_miner.py
@@ -11,6 +11,7 @@ c19_df = pd.read_csv(url)
 world_c19_df = c19_df[c19_df['location']=='World']
 world_c19_df = world_c19_df[['location','date','total_cases','new_cases','total_deaths','new_deaths']]
 # Export to csv
+os.chdir(os.path.dirname(__file__))
 APP_PATH = str(pathlib.Path(__file__).parent.resolve())
 world_c19_df.to_csv(os.path.join(APP_PATH, 'world_c19.csv'))
 #-----------------------------
