a931f5bbf2111d8616b38ccb31f676c34729a39c
antonio-hickey <antoniohickey99@gmail.com>
2021-05-04 00:27:32 -0400

    fixing key leak

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #-----------------------
 # Free Access Token from https://www.mapbox.com/
 #-----------------------
-token = "pk.eyJ1IjoiYWZoaWNrZXkiLCJhIjoiY2tuaG9xbXcwMHA4ZDJubDc0NGZ3dmVpYSJ9.dgDkc3Jyu19_DWd5qZq-TQ"
+token = '* ! YOUR ACCESS TOKEN !*'
 #-----------------------
 
 #-----------------------
