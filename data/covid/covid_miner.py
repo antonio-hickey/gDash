3fc355ba1304e2d16f652bfb4c56bf1739a99b31
antonio-hickey <antoniohickey99@gmail.com>
2021-06-06 01:10:48 -0400

    cleaned covid data collector

--- data/covid/covid_miner.py
+++ data/covid/covid_miner.py
@@ -1,17 +1,26 @@
-import pandas as pd
-import datetime as dt
-import pathlib
-import json
 import os
-#-----------------------------
-# Covid Data
-#-----------------------------
+import pathlib
+
+import pandas as pd
+
+# Url of dataset
 url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
+
+
+# Render DataFrame
 c19_df = pd.read_csv(url)
-world_c19_df = c19_df[c19_df['location']=='World']
-world_c19_df = world_c19_df[['location','date','total_cases','new_cases','total_deaths','new_deaths']]
+
+
+# Filter data to world data
+world_c19_df = c19_df[c19_df['location'] == 'World']
+
+
+# Cleaning dataset to targeted data
+world_c19_df = world_c19_df[['location', 'date', 'total_cases', 'new_cases',
+                             'total_deaths', 'new_deaths']]
+
+
 # Export to csv
 os.chdir(os.path.dirname(__file__))
 APP_PATH = str(pathlib.Path(__file__).parent.resolve())
 world_c19_df.to_csv(os.path.join(APP_PATH, 'world_c19.csv'))
-#-----------------------------
