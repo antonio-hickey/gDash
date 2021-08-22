3bbc983cc3feb22222423008ec3d1f6451755248
antonio-hickey <antoniohickey99@gmail.com>
2021-08-22 03:50:14 -0400

    Scraper for global conflicts

---
+++ data/events/geoConflict.py
@@ -0,0 +1,55 @@
+"""
+Collecting data on conflict worldwide
+
+Source:
+    - Council on Foriegn Relations
+
+Targeted output data:
+    - [Lat, Lon, Description, Link, Scale, Size]
+
+"""
+
+# Import Modules
+import requests as req
+import json
+import csv
+
+
+URL = "https://microsites-live-backend.cfr.org/conflict/map/json"
+page = req.get(URL)
+response = json.loads(page.content)
+data = response['features']
+rows = []
+for dp in data:
+    cords = (dp['geometry']['coordinates'])
+    lat = (cords[1])
+    lon = (cords[0])
+    title = (dp['properties']['title'])
+    link = ('https://www.cfr.org' + dp['properties']['link'])
+    impact = (dp['properties']['severity']['us']['value'])
+    condition = (dp['properties']['severity']['usConflictStatus']['value'])
+
+    if impact == 'critical':
+        impact = '1'
+    elif impact == 'significant':
+        impact = '2'
+    elif impact == 'limited':
+        impact = '3'
+    
+    if condition == 'worsening':
+        condition = '3'
+    elif condition == 'unchanging':
+        condition = '2'
+    elif condition == 'improving':
+        condition = '1'
+
+    rows.append([lat, lon, title, link, condition, impact])
+
+filename = "geoConflicts.csv"
+cols = ['Lat', 'Lon', 'Title', 'Link', 'condition', 'impact']
+with open(filename, 'a') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(cols)
+    for row in rows:
+        csvwriter.writerow(row)
+
