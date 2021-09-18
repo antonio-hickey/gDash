855b08d1f1396571b9cdb46cbe6f98f5cdbf74a7
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-18 01:55:41 -0400

    Cleaned, fleshed out

--- app/dash_app.py
+++ app/dash_app.py
@@ -1,10 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.express as px
 from dash.dependencies import Input, Output
 
+from app.components import covid, economics, mapbox, population
 from app.config import MAPBOX_TOKEN
 from app.util.links import C19_DF, CE_DF, GDP_DF, POP_DF
 
@@ -16,52 +15,6 @@ application = dash.Dash(
 )
 server = application.server
 app = application
-
-ce_df = pd.read_csv(CE_DF)
-c19_df = pd.read_csv(C19_DF)
-gdp_df = pd.read_csv(GDP_DF)
-pop_df = pd.read_csv(POP_DF)
-
-px.set_mapbox_access_token(MAPBOX_TOKEN)
-fig = px.scatter_mapbox(
-        ce_df,
-        lat=ce_df['Lat'],
-        lon=ce_df['Lon'],
-        hover_name=ce_df['Title'],
-        color=ce_df['Condition'],
-        color_continuous_scale=px.colors.diverging.RdYlGn,
-        size=ce_df['Impact'],
-        size_max=15,
-        zoom=1,
-        mapbox_style="satellite-streets",
-    )
-fig["layout"]["height"] = 780
-fig["layout"].update(paper_bgcolor="#d9d9d9", plot_bgcolor="#d9d9d9")
-fig.update_layout(title_text='Earth\'s Current Events', title_x=0.5, title_font_color='#353535',
-                  yaxis=dict(color='#353535'), xaxis=dict(color='#353535'))
-
-
-c19fig = px.area(c19_df, x="date", y="deaths weekly change", color_discrete_sequence=['#3c6e71'])
-c19fig.add_scatter(
-    x=c19_df["date"],
-    y=c19_df["+2 Sigma"],
-    mode='lines',
-)
-c19fig["layout"][
-    "uirevision"
-] = "The User is always right"
-c19fig["layout"]["height"] = 590
-c19fig["layout"]["yaxis"]["title"] = 'Deaths'
-c19fig["layout"]["xaxis"]["title"] = 'Dates'
-c19fig["layout"]["yaxis"]["gridcolor"] = "#ffffff"
-c19fig["layout"]["xaxis"]["gridcolor"] = "#ffffff"
-c19fig.update_layout(legend=dict(font=dict(color="#353535")))
-c19fig["layout"].update(paper_bgcolor="#ffffff", plot_bgcolor="#d9d9d9")
-c19fig.update_yaxes(title_font=dict(color='#353535'))
-c19fig.update_xaxes(title_font=dict(color='#353535'))
-c19fig.update_layout(title_text='COVID-19 Death\'s Global', title_x=0.5, title_font_color='#353535',
-                     yaxis=dict(color='#353535'), xaxis=dict(color='#353535'))
-
 
 app.layout = html.Div(
     id="root",
@@ -88,7 +41,7 @@ app.layout = html.Div(
                             children=[
                                 dcc.Graph(
                                     id="map",
-                                    figure=fig,
+                                    figure=mapbox.figure(CE_DF, MAPBOX_TOKEN),
                                 ),
                             ],
                         ),
@@ -112,8 +65,8 @@ app.layout = html.Div(
                                     "value": "pop",
                                 },
                                 {
-                                    "label": "Recent Major Events",
-                                    "value": "rme",
+                                    "label": "Upcoming Elections",
+                                    "value": "elec",
                                 },
                             ],
                             value="econ",
@@ -143,85 +96,11 @@ app.layout = html.Div(
 )
 def data_display(chart_dropdown):
     if chart_dropdown == "covid":
-
-        c19fig = px.area(c19_df, x="date", y="deaths weekly change", color_discrete_sequence=['#3c6e71'])
-
-        c19fig.add_scatter(
-         x=c19_df["date"],
-         y=c19_df["+2 Sigma"],
-         mode='lines',
-         line=dict(color='#d40f33'),
-         name='+4 Sigma',
-        )
-
-        c19fig.add_scatter(
-         x=c19_df["date"],
-         y=c19_df["-2 Sigma"],
-         mode='lines',
-         line=dict(color='#edac15'),
-         name='+2 Sigma',
-        )
-
-        c19fig["layout"][
-             "uirevision"
-        ] = "The User is always right"
-        c19fig["layout"]["height"] = 590
-        c19fig["layout"]["yaxis"]["title"] = 'Change in Deaths'
-        c19fig["layout"]["xaxis"]["title"] = 'Dates'
-        c19fig["layout"]["yaxis"]["gridcolor"] = "#ffffff"
-        c19fig["layout"]["xaxis"]["gridcolor"] = "#ffffff"
-        c19fig.update_layout(legend=dict(font=dict(color="#353535")))
-        c19fig["layout"].update(paper_bgcolor="#ffffff", plot_bgcolor="#d9d9d9")
-        c19fig.update_yaxes(title_font=dict(color='#353535'))
-        c19fig.update_xaxes(title_font=dict(color='#353535'))
-        c19fig.update_layout(title_text='COVID-19 Change in Deaths', title_x=0.5,
-                             title_font_color='#353535', yaxis=dict(color='#353535'),
-                             xaxis=dict(color='#353535'))
-        return c19fig
-
-    if chart_dropdown == "econ":
-        econFig = px.bar(gdp_df, x="Economy", y="value", color_discrete_sequence=['#3c6e71'])
-        econFig["layout"][
-            "uirevision"
-        ] = "The User is always right"
-        econFig["layout"]["height"] = 590
-        econFig["layout"]["yaxis"]["title"] = 'Dollar\'s'
-        econFig["layout"]["xaxis"]["title"] = 'Nation\'s'
-        econFig["layout"]["yaxis"]["gridcolor"] = '#ffffff'
-        econFig["layout"]["xaxis"]["gridcolor"] = '#ffffff'
-        econFig.update_layout(legend=dict(font=dict(color="#353535")))
-        econFig["layout"].update(paper_bgcolor="#ffffff", plot_bgcolor="#d9d9d9")
-        econFig.update_yaxes(title_font=dict(color='#353535'))
-        econFig.update_xaxes(title_font=dict(color='#353535'))
-        econFig.update_layout(title_text='GDP of Top 20 Nation\'s', title_x=0.5,
-                              title_font_color='#353535', yaxis=dict(color='#353535'),
-                              xaxis=dict(color='#353535'))
-        return econFig
-
-    if chart_dropdown == "pop":
-        relFig = px.pie(pop_df, values='Net Change', names='Country (or dependency)')
-        relFig["layout"][
-            "uirevision"
-        ] = "The User is always right"
-        relFig["layout"]["height"] = 590
-
-        return relFig
-
-    if chart_dropdown == "rme":
-        rmeFig = px.area(c19_df, x="date", y="total_deaths", color_discrete_sequence=['#3c6e71'])
-        rmeFig["layout"][
-            "uirevision"
-        ] = "The User is always right"
-        rmeFig["layout"]["height"] = 590
-        rmeFig["layout"]["yaxis"]["title"] = 'Deaths'
-        rmeFig["layout"]["xaxis"]["title"] = 'Dates'
-        rmeFig["layout"]["yaxis"]["gridcolor"] = '#ffffff'
-        rmeFig["layout"]["xaxis"]["gridcolor"] = '#ffffff'
-        rmeFig.update_layout(legend=dict(font=dict(color="#353535")))
-        rmeFig["layout"].update(paper_bgcolor="#ffffff", plot_bgcolor="#d9d9d9")
-        rmeFig.update_yaxes(title_font=dict(color='#353535'))
-        rmeFig.update_xaxes(title_font=dict(color='#353535'))
-        rmeFig.update_layout(title_text='Recent Major Events', title_x=0.5,
-                             title_font_color='#353535', yaxis=dict(color='#353535'),
-                             xaxis=dict(color='#353535'))
-        return rmeFig
+        return covid.Figure(C19_DF)
+    elif chart_dropdown == "econ":
+        return economics.Figure(GDP_DF)
+    elif chart_dropdown == "pop":
+        return population.Figure(POP_DF)
+    elif chart_dropdown == "elec":
+        # TODO (just a placeholder for now)
+        return covid.Figure(C19_DF)
