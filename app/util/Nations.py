177f4f4455b16b0f89f9454c8992b31b5d71e803
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-18 02:05:39 -0400

    Util to convert nation names

---
+++ app/util/Nations.py
@@ -0,0 +1,44 @@
+def convert_abbreviation(abbrev: str) -> str:
+    if abbrev == 'USA':
+        converted = 'United States'
+    elif abbrev == 'CHN':
+        converted = 'China'
+    elif abbrev == 'JPN':
+        converted = 'Japan'
+    elif abbrev == 'DEU':
+        converted = 'Germany'
+    elif abbrev == 'GBR':
+        converted = 'United Kingdom'
+    elif abbrev == 'IND':
+        converted = 'India'
+    elif abbrev == 'FRA':
+        converted = 'France'
+    elif abbrev == 'ITA':
+        converted = 'Italy'
+    elif abbrev == 'CAN':
+        converted = 'Canada'
+    elif abbrev == 'KOR':
+        converted = 'South Korea'
+    elif abbrev == 'RUS':
+        converted = 'Russia'
+    elif abbrev == 'AUS':
+        converted = 'Australia'
+    elif abbrev == 'BRA':
+        converted = 'Brazil'
+    elif abbrev == 'ESP':
+        converted = 'Spain'
+    elif abbrev == 'MEX':
+        converted = 'Mexico'
+    elif abbrev == 'IDN':
+        converted = 'Indonesia'
+    elif abbrev == 'NLD':
+        converted = 'Netherlands'
+    elif abbrev == 'CHE':
+        converted = 'Switzerland'
+    elif abbrev == 'SAU':
+        converted = 'Saudi Arabia'
+    elif abbrev == 'TUR':
+        converted = 'Turkey'
+
+    return converted
+
