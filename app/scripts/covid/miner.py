1266fa586baee37808274c8a7e813db726a86ba6
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-18 02:02:28 -0400

    Cleaned + Fixing file paths

--- app/scripts/covid/miner.py
+++ app/scripts/covid/miner.py
@@ -1,17 +1,11 @@
-import os
-import pathlib
-
 import pandas as pd
 
 from app.util.links import OWID_COVID
 
 url = OWID_COVID
-
 c19_df = pd.read_csv(url)
 world_c19_df = c19_df[c19_df['location'] == 'World']
 world_c19_df = world_c19_df[['location', 'date', 'total_cases', 'new_cases',
                              'total_deaths', 'new_deaths']]
 
-
-APP_PATH = str(pathlib.Path(__file__).parent.resolve())
-world_c19_df.to_csv(os.path.join(APP_PATH, '../../data/covid/world_c19.csv'))
+world_c19_df.to_csv('app/data/covid/world_c19.csv')
