041423adf8e6557748176ecb43bda361fe6dab3b
Antonio Hickey <antoniohickey99@gmail.com>
2021-09-25 18:43:49 -0400

    Changed covid data link back

--- app/util/links.py
+++ app/util/links.py
@@ -2,6 +2,6 @@ OWID_COVID = "https://raw.githubusercontent.com/owid/covid-19-data/master/public
 CFR_API = "https://microsites-live-backend.cfr.org/conflict/map/json"
 GDACS = "https://www.gdacs.org/xml/rss_24h.xml"
 CE_DF = "https://raw.githubusercontent.com/antonio-hickey/gDash/main/app/data/events/dataset.csv"
-C19_DF = "https://raw.githubusercontent.com/antonio-hickey/gDash/main/app/data/covid/dataset.csv"
+C19_DF = "https://raw.githubusercontent.com/antonio-hickey/gDash/main/app/data/covid/world_c19.csv"
 GDP_DF = "https://raw.githubusercontent.com/antonio-hickey/gDash/main/app/data/economic/gdp.csv"
 POP_DF = "https://raw.githubusercontent.com/antonio-hickey/gDash/main/app/data/demographics/population_by_country_2020.csv"
